c2814874ac9dc9e3fc4b6fc15a1578c8799c2ba2
Ikhbar Nurinsani <ikhbarnuri@gmail.com>
2023-12-17 11:36:21 +0700

    menambah create option form pada teacher untuk class dan periode

--- app/Filament/Resources/TeacherResource/RelationManagers/ClassRoomRelationManager.php
+++ app/Filament/Resources/TeacherResource/RelationManagers/ClassRoomRelationManager.php
@@ -4,13 +4,18 @@ namespace App\Filament\Resources\TeacherResource\RelationManagers;
 
 use App\Models\ClassRoom;
 use App\Models\Periode;
+use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Set;
+use Filament\GlobalSearch\Actions\Action;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
+use Illuminate\Support\Str;
 
 class ClassRoomRelationManager extends RelationManager
 {
@@ -23,13 +28,41 @@ class ClassRoomRelationManager extends RelationManager
                 Select::make('class_room_id')
                     ->label('Select Class')
                     ->options(ClassRoom::all()->pluck('name', 'id'))
+                    ->preload()
                     ->searchable()
-                    ->required(),
+                    ->required()
+                    ->relationship('classRoom', 'name')
+                    ->createOptionForm([
+                        TextInput::make('name')
+                            ->live()
+                            ->afterStateUpdated(fn(Set $set, ?string $state) => $set('slug', Str::slug($state)))
+                            ->required(),
+                        Hidden::make('slug')
+                            ->required(),
+                    ])
+                    ->createOptionAction(function (\Filament\Forms\Components\Actions\Action $action) {
+                        return $action
+                            ->modalHeading('Add Class Room')
+                            ->modalSubmitActionLabel('Add Class Room')
+                            ->modalWidth('2xl');
+                    }),
                 Select::make('periode_id')
                     ->label('Select Periode')
                     ->options(Periode::all()->pluck('name', 'id'))
                     ->searchable()
-                    ->required(),
+                    ->required()
+                    ->relationship('periode', 'name')
+                    ->createOptionForm([
+                        TextInput::make('name')
+                            ->label('Add Periode')
+                            ->required(),
+                    ])
+                    ->createOptionAction(function (\Filament\Forms\Components\Actions\Action $action) {
+                        return $action
+                            ->modalHeading('Add Periode')
+                            ->modalSubmitActionLabel('Add Periode')
+                            ->modalWidth('2xl');
+                    }),
             ]);
     }
 
